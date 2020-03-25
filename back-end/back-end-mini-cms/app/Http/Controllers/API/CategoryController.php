e283d932de32ed17bce7d774e642a14f5f231e79
leandropinheiroo <leandropinheiro98@gmail.com>
2020-03-25 15:12:14 -0300

    Create Back-end Serve and organize folders

---
+++ back-end/back-end-mini-cms/app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->all(),[ 
+            'name' => 'required',
+        ]);
+
+        if($validation->fails()){
+            return response()->json([
+                'error' => true,
+                'messages'  => $validation->errors(),
+            ], 200);
+        }
+        else
+        {
+            $category = new Category;
+            $category->name = $request->input('name');
+            $category->description = $request->input('description');
+            $category->save();
+    
+            return response()->json([
+                'error' => false,
+                'employee'  => $employee,
+            ], 200);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
