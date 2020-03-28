16c84f6ca2a2aa94ff6381f3c4e96e78c953c2f0
leandropinheiroo <leandropinheiro98@gmail.com>
2020-03-28 17:28:13 -0300

    Category Service, set httpOptions to all class

--- front-end/mini-cms/src/app/categories/category.service.ts
+++ front-end/mini-cms/src/app/categories/category.service.ts
@@ -9,51 +9,34 @@ import { environment } from 'src/environments/environment';
 export class CategoryService {
 
   private readonly url: String = environment.api+'category';
+
+  private readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json' // Json type
+      },
+    )
+  };
   constructor(private httpClient: HttpClient) { }
 
+  
+
   getAll(){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'// ,
-        // 'Authorization': 'Basic cDFAZy5jb206YWRtaW4='
-        },
-      )
-    };
-    return this.httpClient.get(`${this.url}`, httpOptions);
+    return this.httpClient.get(`${this.url}`, this.httpOptions);
   }
 
   getId(id){
     if(id != null && id > 0){
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'// ,
-          // 'Authorization': 'Basic cDFAZy5jb206YWRtaW4='
-          },
-        )
-      };
-      return this.httpClient.get(`${this.url+'/'+id}`, httpOptions);
+      return this.httpClient.get(`${this.url+'/'+id}`, this.httpOptions);
     }
   }
 
   save(category: Category){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'// ,
-        // 'Authorization': 'Basic cDFAZy5jb206YWRtaW4='
-        },
-      )
-    };
-    return this.httpClient.post(`${this.url}`, JSON.stringify(category) , httpOptions);
+    return this.httpClient.post(`${this.url}`, JSON.stringify(category) , this.httpOptions);
   } 
   update(category: Category,id:number){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'// ,
-        // 'Authorization': 'Basic cDFAZy5jb206YWRtaW4='
-        },
-      )
-    };
-    return this.httpClient.put(`${this.url+'/'+id}`, JSON.stringify(category) , httpOptions);
+    if(id != null && id > 0){
+      return this.httpClient.put(`${this.url+'/'+id}`, JSON.stringify(category) , this.httpOptions);
+    }
   } 
   
 }
