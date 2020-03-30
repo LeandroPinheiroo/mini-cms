bee8144f90ca65bfb574d636f95a7c558eff22f1
leandropinheiroo <leandropinheiro98@gmail.com>
2020-03-30 14:19:46 -0300

    Create product Model

--- front-end/mini-cms/src/app/core/models.ts
+++ front-end/mini-cms/src/app/core/models.ts
@@ -1,4 +1,10 @@
 export class Category {
-    name: string;
-    description: string;
-  }
+  name: string;
+  description: string;
+}
+export class Product {
+  name: string;
+  price: number;
+  stock: number;
+  description: string;
+}
