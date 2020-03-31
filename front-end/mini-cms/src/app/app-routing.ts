4f1dc28ff3149bcded0ba16a8a8021d7157da257
leandropinheiroo <leandropinheiro98@gmail.com>
2020-03-31 10:01:06 -0300

    Create routes for products list and new

--- front-end/mini-cms/src/app/app-routing.ts
+++ front-end/mini-cms/src/app/app-routing.ts
@@ -1,15 +1,24 @@
+//Modules
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ModuleWithProviders } from '@angular/core';
+//Components
 import { ListCategoriesComponent } from './categories/list-categories/list-categories.component';
 import { NewCategoriesComponent } from './categories/new-categories/new-categories.component';
+import { ListProductsComponent } from './products/list-products/list-products.component';
+import { NewProductsComponent } from './products/new-products/new-products.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    //Categories Routes
     { path: 'categories', component: ListCategoriesComponent },
     { path: 'categories/new', component: NewCategoriesComponent },
     { path: 'categories/edit/:id', component: NewCategoriesComponent },
+    //Products Routes
+    { path: 'products', component: ListProductsComponent },
+    { path: 'products/new', component: NewProductsComponent },
+    { path: 'products/edit/:id', component: NewProductsComponent },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
