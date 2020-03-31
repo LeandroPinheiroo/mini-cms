09cfcbdac7419de3a616292366a1cb890321c474
leandropinheiroo <leandropinheiro98@gmail.com>
2020-03-31 10:02:43 -0300

    Create component new product

--- front-end/mini-cms/src/app/products/products.module.ts
+++ front-end/mini-cms/src/app/products/products.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListProductsComponent } from './list-products/list-products.component';
+import { NewProductsComponent } from './new-products/new-products.component';
 
 
 
 @NgModule({
-  declarations: [ListProductsComponent],
+  declarations: [
+    ListProductsComponent,
+    NewProductsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ListProductsComponent,
+    NewProductsComponent
   ]
 })
 export class ProductsModule { }
