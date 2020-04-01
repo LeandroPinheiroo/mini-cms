386d70765ec3418f9f4b4c3d75d39e5982d3cf65
leandropinheiroo <leandropinheiro98@gmail.com>
2020-04-01 11:44:06 -0300

    Create Back-End-Java project

---
+++ back-end/back-end-java/src/test/java/pinheiro/leandro/backendjava/BackEndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pinheiro.leandro.backendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
