3074552bcc2b4a09be65a53bbdaf1f98f676cf24
leandropinheiroo <leandropinheiro98@gmail.com>
2020-04-07 10:08:59 -0300

    Insert products routes in the index.js

--- back-end/back-end-node/src/index.js
+++ back-end/back-end-node/src/index.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./routes/categories.routes.js")(app);
+require("./routes/products.routes.js")(app);
 // set port, listen for requests
 app.listen(8000, () => {
   console.log("Server is running on port 8000.");
